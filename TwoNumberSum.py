852d14d1a588af9e18ed88d2b11232b5cbf15aa0
Poncho <jplyles1212@gmail.com>
2021-02-15 09:04:48 -0800

    Add sorted list with pointers solution

--- TwoNumberSum.py
+++ TwoNumberSum.py
@@ -1,6 +1,6 @@
 # Olog(n^2) time    O(1) space
 # Brute force
-def TwoNumberSum(array, targetSum):
+def twoNumberSum(array, targetSum):
     for i in range(len(array) - 1):
         firstNum = array[i]
         for j in range(i + 1, len(array)):
@@ -9,4 +9,24 @@ def TwoNumberSum(array, targetSum):
                 return [firstNum, secondNum] 
     return []
 
-print(TwoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
+print(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10)
+
+
+# O(nlogn) time     O(1) space
+# Sorted and using pointers
+def twoNumberSum(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+	
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    return []
+
+print(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
