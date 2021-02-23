2f14659dbe64ff7e36a49171ed363f635b98526f
Poncho <jplyles1212@gmail.com>
2021-02-22 19:07:03 -0800

    Add dictionary method solution

--- TwoNumberSum.py
+++ TwoNumberSum.py
@@ -30,3 +30,16 @@ def twoNumberSum(array, targetSum):
     return []
 
 print(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
+
+# O(n) time     O(n) space
+# Dictionary method
+def twoNumberSum(array, targetSum):
+    nums = {}
+	
+	for num in array:
+		potentialMatch = targetSum - num
+		if potentialMatch in nums:
+			return [potentialMatch, num]
+		else:
+			nums[num] = True
+	return []
