699b5771990954d6c1d78d50dc05f2efd8cf802b
Poncho <jplyles1212@gmail.com>
2021-02-24 18:57:46 -0800

    Add isPalindrome solution

---
+++ isPalindrome.py
@@ -0,0 +1,13 @@
+# O(n) time     O(1) space
+def isPalindrome(string):
+    left = 0
+    right = len(string) - 1
+
+    while left < right:
+        if string[left] == string[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+
+    return True
